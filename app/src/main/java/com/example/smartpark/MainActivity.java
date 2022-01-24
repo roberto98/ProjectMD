6af86133e3ac72ff959302b8eb31af6b80c8dd88
Roberto <roby.divia@gmail.com>
2022-01-24 21:08:00 +0100

    v1.0 3 Dots Creazione e gestione delle opzioni (3 puntini) nella barra di navigazione in alto

--- app/src/main/java/com/example/smartpark/MainActivity.java
+++ app/src/main/java/com/example/smartpark/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.smartpark;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // ------------------------------------------------------
+    // Gestione del Menù a 3 puntini nella barra in alto
+    // ------------------------------------------------------
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_opt_1) {
+            Log.d("/tag", "ciao1");
+            return true;
+        }
+
+        if (id == R.id.action_opt_2) {
+            Log.d("/tag", "ciao2");
+            return true;
+        }
+
+        if (id == R.id.action_opt_3) {
+            Log.d("/tag", "ciao3");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    // ------------------------------------------------------
+    // Work in progress
+    // ------------------------------------------------------
+
 }
