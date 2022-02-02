0d484cefbda127871398a1ff8d1797d2ab19cce0
Edoardo <57142196+meruemo94@users.noreply.github.com>
2022-02-02 17:04:24 +0100

    Take a picture

--- app/src/main/java/com/example/smartpark/PositionDetails.java
+++ app/src/main/java/com/example/smartpark/PositionDetails.java
@@ -1,17 +1,132 @@
 package com.example.smartpark;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PositionDetails extends AppCompatActivity {
     private static final String LOG_TAG = "/TAG/"+PositionDetails.class.getSimpleName();
+    
+    ImageView imageView;
+    String time,time2;
+    boolean bool=false;
+    Bitmap imageBitmap;
+    Intent tmpIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_position_details);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = time;//or time2
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+            }
+
+        });
+        // hold down to delete the image
+        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                imageView.setImageDrawable(null);
+                return true;
+            }
+
+        });
+    }
+
+    private void saveToInternalStorage(Bitmap bitmapImage){
+        ContextWrapper context = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = context.getFilesDir();
+        // Create imageDir
+        File mypath=new File(directory,"new.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // return directory.getAbsolutePath();
+    }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Bundle extras = data.getExtras();
+                        imageBitmap = (Bitmap) extras.get("data");
+
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        saveToInternalStorage(imageBitmap);
+
+                        if(bool == false) {
+                            time = dateFormat.format(new Date());
+                            imageView = (ImageView) findViewById(R.id.imageView);
+
+                            imageView.setImageBitmap(imageBitmap);
+
+                            bool=true;
+                        }
+                        else{
+                            //time2 = dateFormat.format(new Date());
+                            imageView = (ImageView) findViewById(R.id.imageView);
+                            Drawable imageBitmap_tmp = imageView.getDrawable();
+                            imageView.setImageBitmap(imageBitmap);
+                        }
+                        Toast.makeText(getBaseContext(), "saved photo", Toast.LENGTH_SHORT).show();
+                        //imageView.setRotation(90)
+                    }
+                }
+            });
+
+    public void TakePicture(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        tmpIntent =intent;
+        someActivityResultLauncher.launch(intent);
+        ActivityCompat.requestPermissions(PositionDetails.this,new String[]{Manifest.permission.CAMERA},1);
     }
 
     @Override
