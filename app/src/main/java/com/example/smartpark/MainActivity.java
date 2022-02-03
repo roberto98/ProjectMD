0b1c02455583a52c51feb24350c09823eb4adc07
Edoardo <57142196+meruemo94@users.noreply.github.com>
2022-02-03 20:02:10 +0100

    Get Current Location (manual location), foreground location;

--- app/src/main/java/com/example/smartpark/MainActivity.java
+++ app/src/main/java/com/example/smartpark/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.smartpark;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+
+
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -11,15 +21,62 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 import java.util.Calendar;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private static final String LOG_TAG = "//tag";
+    private LocationManager locationManager;
+    private TextView txtLat;
+    private Location location;
+    private double latitude=0;
+    private double longitude=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtLat = (TextView) findViewById(R.id.park_time);
+        // https://javapapers.com/android/get-current-location-in-android/
+        //  https://www.youtube.com/watch?v=-dO23oDmAaE
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, 100);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0 , this);
+
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //txtLat = (TextView) findViewById(R.id.park_time);
+        //txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        //locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude", "disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude", "enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude", "status");
     }
 
     // ------------------------------------------------------
@@ -65,26 +122,25 @@ public class MainActivity extends AppCompatActivity {
     // Work in progress
     // ------------------------------------------------------
 
-    public void startAutomaticSaving(){
-        Intent i=new Intent(this, AutomaticSaving.class);
+    public void startAutomaticSaving() {
+        Intent i = new Intent(this, AutomaticSaving.class);
         startActivity(i);
     }
 
-    public void startPosDetails(View v){
-        Intent i=new Intent(this, PositionDetails.class);
+    public void startPosDetails(View v) {
+        Intent i = new Intent(this, PositionDetails.class);
         startActivity(i);
     }
 
-    public void ManualSaving(View v){
+    public void ManualSaving(View v) {
+        Log.d(LOG_TAG, "5------------------");
         Date currentTime = Calendar.getInstance().getTime();
 
         TextView park_time = findViewById(R.id.park_time);
         park_time.setText("Park's time: "+currentTime);
 
         TextView coord_txt = findViewById(R.id.coord_txt);
-        String coordinates = "123"; //TODO: implementare funzione gps
-        coord_txt.setText("Coordinates: "+coordinates);
-
+        coord_txt.setText("Latitude:"  + latitude +", Longitude:" + longitude);
     }
 
     // ------------------------------------------------------
@@ -93,6 +149,7 @@ public class MainActivity extends AppCompatActivity {
 
     //https://stackoverflow.com/questions/42595585/how-to-turn-on-gps-sensor-and-get-location-from-a-service-in-android-m
     // https://stackoverflow.com/questions/1513485/how-do-i-get-the-current-gps-location-programmatically-in-android
+
     private void getGPS(){
 
     }
